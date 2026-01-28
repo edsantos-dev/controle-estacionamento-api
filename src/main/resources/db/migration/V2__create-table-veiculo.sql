603c55738bd8046311f96110e4a10b0a846e5000
imbassado <edualansatos@gmail.com>
2026-01-28 00:18:38 -0300

    Erro com sintaxe SQL ajustado na V2 da Migration.

--- src/main/resources/db/migration/V2__create-table-veiculo.sql
+++ src/main/resources/db/migration/V2__create-table-veiculo.sql
@@ -1,7 +1,7 @@
 create table veiculo (
     id bigint not null auto_increment,
     placa varchar(10) not null,
-    tipo varchar(20) not null.
+    tipo varchar(20) not null,
 
     constraint pk_veiculo primary key (id),
     constraint uk_veiculo_placa unique (placa)
